16b666dbf2fd4da26b9b5e44cf9588f1331bac1c
Leina Gray <abad.alleina@gmail.com>
2023-07-25 00:48:18 +0800

    add: design in terminal by: Edrick L. De Villa

--- Package.py
+++ Package.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 class Package:
 
     def __init__(self, package_id, address, city, state, package_zip, deadline, weight,
@@ -19,14 +20,19 @@ class Package:
     # This method simply prints out the package
     def print_package(self, status):
         if status == "Undelivered":
-            print(str(self.package_id) + ": " + self.address + ", " + self.city + ", " + self.state + ", " + str(
-                self.package_zip) + ". Deadline: " + str(
-                self.deadline.time()) + ". Status: " + status + ".")
+            print(colored(str(self.package_id) + ': ' + self.address + ', ' + self.city + ", " + self.state + ", " + str(
+                self.package_zip) + ".", attrs=['underline']))
+            print(colored('   Deadline: ', attrs=['bold']) + str(self.deadline.time()) + ".")
+            print(colored('   Status: ', attrs=['bold']) + colored(status + ".\n", 'red', attrs=['bold']))
         elif status == "Loaded on truck 2":
-            print(str(self.package_id) + ": " + self.address + ", " + self.city + ", " + self.state + ", " + str(
-                self.package_zip) + ". Deadline: " + str(
-                self.deadline.time()) + ". Status: " + status + " @ " + str(self.time_loaded.time()))
+            print(colored(str(self.package_id) + ': ' + self.address + ', ' + self.city + ", " + self.state + ", " + str(
+                self.package_zip) + ".", attrs=['underline']))
+            print(colored('   Deadline: ', attrs=['bold']) + str(self.deadline.time()) + ".")
+            print(colored('   Status: ', attrs=['bold']) + colored(status + ".", 'yellow', attrs=['bold']))
+            print(colored('   Time Loaded: ', attrs=['bold']) + str(self.time_loaded.time()) + "\n")
         else:
-            print(str(self.package_id) + ": " + self.address + ", " + self.city + ", " + self.state + ", " + str(
-                self.package_zip) + ". Deadline: " + str(
-                self.deadline.time()) + ". Status: " + status + " @ " + str(self.time_delivered.time()))
+            print(colored(str(self.package_id) + ': ' + self.address + ', ' + self.city + ", " + self.state + ", " + str(
+                self.package_zip) + ".", attrs=['underline']))
+            print(colored('   Deadline: ', attrs=['bold']) + str(self.deadline.time()) + ". ")
+            print(colored('   Status: ', attrs=['bold']) + colored(status + ". ", 'green', attrs=['bold']))
+            print(colored('   Time Delivered: ', attrs=['bold']) + str(self.time_delivered.time()) + "\n")
